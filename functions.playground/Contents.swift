7654eec3a3d9f81190d2f1089b9056fc1b302f5c
codyc-xyz <crabmarket56@gmail.com>
2024-03-05 15:45:42 +0700

    Removing need to use arg name in func call

--- functions.playground/Contents.swift
+++ functions.playground/Contents.swift
@@ -15,3 +15,16 @@ func newPlusTwo(x: Int) -> Int {
 }
 
 newPlusTwo(x: 30)
+
+func customAdd(v1: Int, v2: Int) -> Int {
+    return v1 + v2
+}
+
+let customAdded = customAdd(v1: 20, v2: 22)
+
+// adding _ arg: removes necessity to name args in function call
+func customMinus(_ v1: Int, _ v2: Int) -> Int {
+    return v1 - v2
+}
+
+let customSubtracted = customMinus(24,22)
