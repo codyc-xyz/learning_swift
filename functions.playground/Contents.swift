7c24d5d715cdb1d7a7dbc7a3625c6c31589e92d1
codyc-xyz <crabmarket56@gmail.com>
2024-03-05 15:39:04 +0700

    Basic function structure

--- functions.playground/Contents.swift
+++ functions.playground/Contents.swift
@@ -1,2 +1,11 @@
 import Foundation
 
+func noArgumentsAndNoReturnValue() {
+    "Does Nothing"
+}
+noArgumentsAndNoReturnValue()
+
+func plusTwo(x: Int) {
+    let newValue = x + 2
+}
+plusTwo(x: 30)
