7a475e025cd011b791807815b6f075a6e1a8bf36
codyc-xyz <crabmarket56@gmail.com>
2024-03-05 14:08:33 +0700

    Explore differences between class-based and structure based array variables

--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -11,6 +11,10 @@ myName,
 yourName
 ]
 
+// a copy made with let is mutable, will not impact old array as it is a structure
+var newNames = names
+newNames.append("Baz")
+
 names.append("New Name")
 
 // class instances (e.g. NSMutable Array) differ from structures (let, var)
@@ -23,3 +27,8 @@ let oldArray = NSMutableArray(
 )
 oldArray.add("Baz")
 print(oldArray)
+
+// altering a copy of a class instance will alter both the new and original class
+var newArray = oldArray
+newArray.add("Bez")
+print(newArray, oldArray)
