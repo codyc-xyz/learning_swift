ee11d9a3cd5a8ae0826cedb7c4af3f8926f27c4a
codyc-xyz <crabmarket56@gmail.com>
2024-03-05 15:08:18 +0700

    if, else if, else

--- if_else.playground/Contents.swift
+++ if_else.playground/Contents.swift
@@ -7,5 +7,13 @@ let yourAge = 30
 
 if myName == "Cody" {
     "Your name is \(myName)"
+} else {
+    "Your name is not \(myName)"
 }
 
+if myAge == 26 {
+    "My age is \(myAge)"
+}
+else if myAge == 30 {
+    "My age is 30"
+}
