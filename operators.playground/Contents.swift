008da3137c0778eaf1b789f9a5f8ae9edd210fc3
codyc-xyz <crabmarket56@gmail.com>
2024-03-05 14:25:04 +0700

    greater, less, equal

--- operators.playground/Contents.swift
+++ operators.playground/Contents.swift
@@ -1,3 +1,14 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let myAge = 26
+let yourAge = 30
+
+if myAge > yourAge {
+    "I am older"
+}
+else if yourAge > myAge {
+    "You are older"
+}
+else {
+    "We are the same age"
+}
