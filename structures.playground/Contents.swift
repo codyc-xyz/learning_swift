89587e7b3ffabf6e4bb67aebeeb5d0014147b917
codyc-xyz <crabmarket56@gmail.com>
2024-03-06 14:05:44 +0700

    Init structures

---
+++ structures.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+// structures are a way to group data together
+// they are value types - when you assign an instance of a structure to another variable, the data inside gets copied over. It is not the same reference
+
+struct person {
+    
+}
+
+let foo = person()
