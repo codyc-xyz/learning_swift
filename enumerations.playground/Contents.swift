11cc64ae456cff6b06328a152747e3661f3e8745
codyc-xyz <crabmarket56@gmail.com>
2024-03-06 15:01:45 +0700

    Add switch statement

--- enumerations.playground/Contents.swift
+++ enumerations.playground/Contents.swift
@@ -5,3 +5,20 @@ enum Animals {
 }
 
 let cat = Animals.cat
+
+if cat == Animals.cat {
+    "This is a cat"
+} else if cat == Animals.dog {
+    "This is not a cat"
+} else {
+    "This is a rabbit"
+}
+
+switch cat {
+case .cat:
+    "This is a cat"
+case .dog:
+    "This is a dog"
+case .rabbit:
+    "This is a rabbit"
+}
