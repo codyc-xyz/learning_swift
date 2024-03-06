93bf60ea7a8c23bba3048ee08ca49e826a5d61b0
codyc-xyz <crabmarket56@gmail.com>
2024-03-06 14:01:57 +0700

    In-line creation of closure vs declarative

--- closures.playground/Contents.swift
+++ closures.playground/Contents.swift
@@ -27,3 +27,19 @@ ages.sorted(by: {(_ lhs: Int, _ rhs: Int) -> Bool in
 
 ages.sorted(by: <)
 ages.sorted(by: >)
+
+func addTenTo(_ val: Int) -> Int {
+    return val + 10
+}
+
+func addTwentyTo(_ val: Int) -> Int {
+    return val + 20
+}
+
+func doAddition(on val: Int, using function: (Int) -> Int) -> Int {
+    return function(val)
+}
+
+doAddition(on: 20, using: addTenTo(_:))
+
+doAddition(on: 20, using: { (value) in value + 30 })
