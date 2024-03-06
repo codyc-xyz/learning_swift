4510a0d67ea9bfc2f570f34e95d461aea413e6a0
codyc-xyz <crabmarket56@gmail.com>
2024-03-06 15:12:43 +0700

    Dynamically created enum values

--- enumerations.playground/Contents.swift
+++ enumerations.playground/Contents.swift
@@ -17,8 +17,35 @@ if cat == Animals.cat {
 switch cat {
 case .cat:
     "This is a cat"
+    break
 case .dog:
     "This is a dog"
+    break
 case .rabbit:
     "This is a rabbit"
+    break
+}
+
+// enums in swift can have associated values that are dynamically created at time of creation
+
+enum Shortcut {
+    case fileOrFolder(path: URL, name: String), wwwUrl(path: URL), song(artist: String, songName: String)
+}
+
+let wwwApple = Shortcut.wwwUrl(
+    path: URL(string: "https://apple.com")!
+)
+
+switch wwwApple {
+case .fileOrFolder(path: let path, name: let name):
+    path
+    name
+    break
+case .wwwUrl(path: let path):
+    path
+    break
+case .song(artist: let artist, songName: let songName):
+    artist
+    songName
+    break
 }
