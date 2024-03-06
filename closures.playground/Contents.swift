c9523c00fac21809bae18f758bd354a7a07c7eca
codyc-xyz <crabmarket56@gmail.com>
2024-03-06 13:37:52 +0700

    Pass closure to function

--- closures.playground/Contents.swift
+++ closures.playground/Contents.swift
@@ -1,9 +1,21 @@
 import Foundation
 
 // closures are in-line functions
+// useful because you can pass them to another function
 
 let add: (Int, Int) -> Int 
     = {
         (_ lhs: Int, _ rhs: Int) -> Int in
         return lhs + rhs
     }
+add(20, 30)
+
+func customAdd(
+    _ lhs: Int,
+    _ rhs: Int,
+    using function: (Int, Int) -> Int
+) -> Int {
+    return function(lhs, rhs)
+}
+
+customAdd(22, 33, using: add)
