574a9ee09ad4a8f95729e07a3befadddb2574096
codyc-xyz <crabmarket56@gmail.com>
2024-03-07 13:37:51 +0700

    move generic signature to end of func for readability

--- generics.playground/Contents.swift
+++ generics.playground/Contents.swift
@@ -9,3 +9,11 @@ func perform<N: Numeric>(_ op: (N, N) -> N,
 }
 
 perform(+, on: 10, and: 20)
+
+func performSignatureEnd<N>(_ op: (N, N) -> N,
+             on lhs: N, and rhs: N
+) -> N where N: Numeric {
+    return op(lhs, rhs)
+}
+
+performSignatureEnd(+, on: 10, and: 20)
