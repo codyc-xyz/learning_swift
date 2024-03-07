790fdfa59105afc49f7893396cc9b1d8bda63c9f
codyc-xyz <crabmarket56@gmail.com>
2024-03-07 11:07:20 +0700

    Init protocols

---
+++ protocols.playground/Contents.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+// Protocols are a special kind of object -> similar to interfaces/mixins
+// You create a set of rules that objects which conform to a protocol have to follow
+
