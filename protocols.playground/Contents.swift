82ff96a5f4ade910d70b3ff279b1a015e19dc070
codyc-xyz <crabmarket56@gmail.com>
2024-03-07 11:22:22 +0700

    Getters and setters

--- protocols.playground/Contents.swift
+++ protocols.playground/Contents.swift
@@ -37,3 +37,30 @@ extension canJump {
     }
 }
 
+// protocols cannot be instantiated, need to make e.g. a struct
+struct Cat: canJump {
+    // cat will have jump function by default, but can change it if you want e.g.
+//    func jump() {
+//       -> your altered jump function
+//    }
+}
+
+let whiskers = Cat()
+whiskers.jump()
+
+protocol hasName {
+    // this protocol is saying any object that conforms to hasName, has to have a name var that at least has a getter
+    // a getter is just saying the name variable is declared in the inherited object
+    var name: String { get }
+    // set means it has to be a var because the protocol is saying this variable can be set rather than just get (let -> non-alterable)
+    var age: Int { get set }
+}
+
+struct Dog: hasName {
+    let name: String
+    var age: Int
+}
+
+var woof = Dog(name: "Spot", age: 5)
+// because age has a set, you can increase age, because name is just get, you could possible change if the Dog struct was a var, but since it is let, cannot
+woof.age += 1
