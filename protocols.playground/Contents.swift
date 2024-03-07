a75405324ec4d14d1187aeb4afe9f989fd5d411f
codyc-xyz <crabmarket56@gmail.com>
2024-03-07 11:10:03 +0700

    protocol inheritance

--- protocols.playground/Contents.swift
+++ protocols.playground/Contents.swift
@@ -3,3 +3,19 @@ import Foundation
 // Protocols are a special kind of object -> similar to interfaces/mixins
 // You create a set of rules that objects which conform to a protocol have to follow
 
+protocol canBreathe {
+    func breathe()
+}
+
+// structs allow inheritance from protocols
+struct Animal: canBreathe {
+    func breathe() {
+        "Animal breathing..."
+    }
+}
+
+struct Person: canBreathe {
+    func breathe() {
+        "Person breathing..."
+    }
+}
