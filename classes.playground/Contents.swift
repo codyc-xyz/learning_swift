bc8c62ee24f573748fb2c4ff5d65c0d066978159
codyc-xyz <crabmarket56@gmail.com>
2024-03-07 10:18:01 +0700

    Add simple mutating class

--- classes.playground/Contents.swift
+++ classes.playground/Contents.swift
@@ -1,3 +1,19 @@
 import Foundation
 
-// category of storage space in memory - like structures but not value types -> reference types -> allow mutability without prefixing things
+// category of storage space in memory - like structures but not value types -> reference types -> allow mutability without prefixing things with "mutating" syntax
+
+class Person {
+    var name: String
+    var age: Int
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    func increaseAge() {
+        self.age += 1
+    }
+}
+
+let foo = Person(name: "Foo", age: 22)
+foo.increaseAge()
+foo.age
