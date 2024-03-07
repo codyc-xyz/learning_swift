935d129acf54f16ed1f751c7713a3d9a2939f920
codyc-xyz <crabmarket56@gmail.com>
2024-03-07 12:11:29 +0700

    Add basic extension

--- extensions.playground/Contents.swift
+++ extensions.playground/Contents.swift
@@ -1,3 +1,12 @@
 import Foundation
 
 // extensions add functionality to existing types -> extend existing types
+
+extension Int {
+    func plusTwo() -> Int {
+        return self + 2
+    }
+}
+ let two = 2
+two.plusTwo()
+two
