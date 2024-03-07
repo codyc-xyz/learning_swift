9bf89814c13fbc72be787d12c0c44e2a9735d922
codyc-xyz <crabmarket56@gmail.com>
2024-03-07 10:21:35 +0700

    classes are reference types

--- classes.playground/Contents.swift
+++ classes.playground/Contents.swift
@@ -17,3 +17,17 @@ class Person {
 let foo = Person(name: "Foo", age: 22)
 foo.increaseAge()
 foo.age
+
+// if bar = foo, they point to the same thing in memory, this is what is meant by reference type
+let bar = foo
+bar.increaseAge()
+foo.age
+bar.age
+
+// check if two references point to the same thing in memory with '==='
+
+if foo === bar {
+    "foo and bar point to the same thing in memory"
+} else {
+    "they don't point to the same thing in memory"
+}
