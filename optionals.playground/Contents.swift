d90e330666958e84d0d02f4a273c2f7e73bde9cc
codyc-xyz <crabmarket56@gmail.com>
2024-03-12 11:32:17 +0700

    add function with optional input

--- optionals.playground/Contents.swift
+++ optionals.playground/Contents.swift
@@ -1,3 +1,13 @@
 import Foundation
 
 // optionals indicate a value that may or may not be present
+
+func multiplyByTwo(_ value: Int? = nil) -> Int {
+    if let value {
+        return value * 2
+    }
+    else {
+        return 0
+    }
+}
+multiplyByTwo()
