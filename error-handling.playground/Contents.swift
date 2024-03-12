8b0a1475bba711a5555bebd5d204ac366b33f40e
codyc-xyz <crabmarket56@gmail.com>
2024-03-12 13:32:21 +0700

    Basic error handling in struct

--- error-handling.playground/Contents.swift
+++ error-handling.playground/Contents.swift
@@ -2,3 +2,27 @@ import Foundation
 
 // the point of errors and exceptions are to tell the caller of the code that something has gone wrong
 // errors and exceptions referred to as errors in swift
+
+struct Person {
+    let firstName: String?
+    let lastName: String?
+    
+    enum PersonErrors: Error {
+        case firstNameIsNil
+        case lastNameIsNil
+        case bothNamesAreNil
+    }
+    
+    func getFullName() throws -> String? {
+        switch (firstName, lastName) {
+        case (.none, .none):
+            throw PersonErrors.bothNamesAreNil
+        case (.some, .none):
+            throw PersonErrors.lastNameIsNil
+        case (.none, .some):
+            throw PersonErrors.firstNameIsNil
+        case (.some, .some):
+            throw PersonErrors.bothNamesAreNil
+        }
+    }
+}
