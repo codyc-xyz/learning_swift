df530a29ae93f87e7ea13af1b13ec6892c0d4638
codyc-xyz <crabmarket56@gmail.com>
2024-03-13 16:19:53 +0700

    Equatable enums

--- equality-and-hashing.playground/Contents.swift
+++ equality-and-hashing.playground/Contents.swift
@@ -34,3 +34,19 @@ if foo == bar {
 } else {
     "Foo and Bar are not equal"
 }
+
+// can also implement custom equality on enums
+enum AnimalType: Equatable {
+    case dog(breed: String)
+    case cat(breed: String)
+}
+
+let dog = AnimalType.dog(breed: "Retriever")
+let dog2 = AnimalType.dog(breed: "Retriever")
+
+// swift equatable looks at the vals of dog and dog2 and determines they are equal because the vals are the same, even if the instance is different
+if dog == dog2 {
+    "dog and dog2 are identical"
+} else {
+    "dog and dog2 are not identical"
+}
