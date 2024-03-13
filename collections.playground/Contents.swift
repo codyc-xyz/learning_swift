c7402f8e2c0e34441d091608c92a063c98c82c90
codyc-xyz <crabmarket56@gmail.com>
2024-03-13 14:34:53 +0700

    Mutating arrays

--- collections.playground/Contents.swift
+++ collections.playground/Contents.swift
@@ -9,3 +9,10 @@ numbers.first
 numbers.last
 numbers.count
 numbers.map(-)
+
+// arrays can be mutated so long as they are 'vars'
+
+var mutatingArray: [Int] = [1,2,3,4]
+mutatingArray.append(5)
+mutatingArray.insert(0, at: 0)
+mutatingArray
