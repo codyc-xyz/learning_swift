dd6203a00b8c522fb0ce8eb1e83d78da18896d7b
codyc-xyz <crabmarket56@gmail.com>
2024-03-13 16:05:26 +0700

    Example of equatable conforming struct behavior

--- equality-and-hashing.playground/Contents.swift
+++ equality-and-hashing.playground/Contents.swift
@@ -1,4 +1,21 @@
 import Foundation
 
-// equality in swift is provided via a protocol called Equitable
-// any object conforming to this protocol has to have a static func '==' with two arguments, lhs and rhs
+// equality in swift is provided via a protocol called Equatable
+// any object conforming to this protocol has to be able to handle a static func '==' with two arguments, lhs and rhs with all items of the object (e.g. the properties have to be something that can be equated in swift)
+
+struct Person: Equatable {
+    let id: UUID
+    let name: String
+    let age: Int
+}
+
+let foo = Person(id: UUID(), name: "Foo", age: 22)
+let bar = Person(id: foo.id, name: "Bar", age: 32)
+
+// even though foo and bar have the same id, they are not considered equal due to the rest of the properties
+if foo == bar {
+    "Foo and Bar are equal"
+} else {
+    "Foo and Bar are not equal"
+}
+
