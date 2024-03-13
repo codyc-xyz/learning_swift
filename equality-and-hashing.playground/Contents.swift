13363708689d44d7483296bf6d4c16c368d066ff
codyc-xyz <crabmarket56@gmail.com>
2024-03-13 16:00:09 +0700

    Init equality and hashing

---
+++ equality-and-hashing.playground/Contents.swift
@@ -0,0 +1,4 @@
+import Foundation
+
+// equality in swift is provided via a protocol called Equitable
+// any object conforming to this protocol has to have a static func '==' with two arguments, lhs and rhs
