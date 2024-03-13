2866814d51c6a2757e4db6fb733b9c295c601676
codyc-xyz <crabmarket56@gmail.com>
2024-03-13 14:33:12 +0700

    Simple array and methods

--- collections.playground/Contents.swift
+++ collections.playground/Contents.swift
@@ -2,4 +2,10 @@ import Foundation
 
 // collections are a collection of items (e.g. numbers) -> also called arrays or dicts
 
+// arrays
 
+let numbers: [Int] = [1,2,3,4]
+numbers.first
+numbers.last
+numbers.count
+numbers.map(-)
