5c80b8e6538df1ba70f9c3d407e8fdd026043c2e
codyc-xyz <crabmarket56@gmail.com>
2024-03-25 20:56:21 +0700

    Enable async code

--- asynchronous-programming.playground/Contents.swift
+++ asynchronous-programming.playground/Contents.swift
@@ -2,5 +2,7 @@ import Foundation
 import PlaygroundSupport
 import _Concurrency
 
-
-//
+// This line of code configures the playground to allow for asynchronous code to run indefinitely. By default, a Swift Playground executes code in the page and then stops, assuming the execution is complete. This behavior is fine for synchronous code that runs to completion immediately. However, for asynchronous operations (like networking calls, timers, or any operation that completes in the future), you need the playground to keep running so that these async operations have time to complete.
+PlaygroundPage
+    .current
+    .needsIndefiniteExecution = true
